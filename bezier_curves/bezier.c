210aa90d30aed5cf0b83323fc3e707136a7191dd
Marcelvla <marcelvdla@gmail.com>
2018-11-12 15:09:33 +0100

    added bezier curves assigment'

---
+++ bezier_curves/bezier.c
@@ -0,0 +1,84 @@
+/* Computer Graphics, Assignment, Bezier curves
+ * Filename ........ bezier.c
+ * Description ..... Bezier curves
+ * Date ............ 22.07.2009
+ * Created by ...... Paul Melis
+ *
+ * Student name ....
+ * Student email ...
+ * Collegekaart ....
+ * Date ............
+ * Comments ........
+ *
+ *
+ * (always fill in these fields before submitting!!)
+ */
+
+#include <math.h>
+#include "bezier.h"
+#include <stdio.h>
+
+/* Given a Bezier curve defined by the 'num_points' control points
+ * in 'p' compute the position of the point on the curve for parameter
+ * value 'u'.
+ *
+ * Return the x and y values of the point by setting *x and *y,
+ * respectively.
+ */
+
+void
+evaluate_bezier_curve(float *x, float *y, control_point p[], int num_points, float u)
+{
+    *x = 0.0;
+    *y = 0.0;
+}
+
+/* Draw a Bezier curve defined by the control points in p[], which
+ * will contain 'num_points' points.
+ *
+ *
+ * The 'num_segments' parameter determines the "discretization" of the Bezier
+ * curve and is the number of straight line segments that should be used
+ * to approximate the curve.
+ *
+ * Call evaluate_bezier_curve() to compute the necessary points on
+ * the curve.
+ *
+ * You will have to create a GLfloat array of the right size, filled with the
+ * vertices in the appropriate format and bind this to the buffer.
+ */
+
+void
+draw_bezier_curve(int num_segments, control_point p[], int num_points)
+{
+    GLuint buffer[1];
+
+    /* Write your own code to create and fill the array here. */
+
+
+    // This creates the VBO and binds an array to it.
+    glGenBuffers(1, buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, *buffer);
+    glBufferData(GL_ARRAY_BUFFER, 0 /* Fill in the right size here*/,
+                 NULL /*Fill in the pointer to the array*/, GL_STATIC_DRAW);
+
+    // This tells OpenGL to draw what is in the buffer as a Line Strip.
+    glEnableClientState(GL_VERTEX_ARRAY);
+    glVertexPointer(2, GL_FLOAT, 0, 0);
+    glDrawArrays(GL_LINE_STRIP, 0, 0/* Fill in the number of steps to be drawn*/);
+    glDisableClientState(GL_VERTEX_ARRAY);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glDeleteBuffers(1, buffer);
+}
+
+/* Find the intersection of a cubic Bezier curve with the line X=x.
+   Return 1 if an intersection was found and place the corresponding y
+   value in *y.
+   Return 0 if no intersection exists.
+*/
+
+int
+intersect_cubic_bezier_curve(float *y, control_point p[], float x)
+{
+    return 0;
+}
