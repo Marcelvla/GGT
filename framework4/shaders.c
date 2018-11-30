517192b6a400eb23068c61b47f2c342ca0c494ab
ScrubMasterLenny <40162979+ScrubMasterLenny@users.noreply.github.com>
2018-11-30 20:57:53 +0100

    comments on shaders.c

--- framework4/shaders.c
+++ framework4/shaders.c
@@ -39,6 +39,7 @@ shade_matte(intersection_point ip)
     float intensity = scene_ambient_light;
 
     for (int i = 0; i < scene_num_lights; i++) {
+        // n x l_i
         vec3 vec_light = v3_subtract(scene_lights[i].position, ip.p);
         vec_light = v3_normalize(vec_light);
         float light_cont = v3_dotprod(ip.n, vec_light) * scene_lights[i].intensity;
@@ -46,12 +47,14 @@ shade_matte(intersection_point ip)
         int shadow = shadow_check(v3_add(ip.p, v3_create(0.001, 0.001, 0.001)), vec_light);
         shadow += shadow_check(v3_add(ip.p, v3_create(-0.001, -0.001, -0.001)), vec_light);
 
-        if (light_cont > 0 && shadow < 2 ) {
+        // n x l_i must be bigger than 0, otherwise the light doesn't shine on the surface
+        // one of the two shadow checks must fail, otherwise the point is in the shadows
+        if (light_cont > 0 && shadow < 2) {
             intensity += light_cont;
         }
     }
     return v3_create(intensity, intensity, intensity);
-}
+});
 
 vec3
 shade_blinn_phong(intersection_point ip)
@@ -61,34 +64,36 @@ shade_blinn_phong(intersection_point ip)
     float alpha = 50;
     vec3 c_d = v3_create(1,0,0);
     vec3 c_s = v3_create(1,1,1);
-
     vec3 n = ip.n;
 
     float cd_component = 0;
     float cs_component = 0;
 
     for (int i = 0; i < scene_num_lights; i++) {
+        // n x l_i
         vec3 vec_light = v3_subtract(scene_lights[i].position, ip.p);
         vec_light = v3_normalize(vec_light);
         float light_cont = v3_dotprod(ip.n, vec_light) * scene_lights[i].intensity;
 
-        vec3 h = v3_normalize(v3_subtract(scene_lights[i].position, ip.i));
-
         int shadow = shadow_check(v3_add(ip.p, v3_create(0.001, 0.001, 0.001)), vec_light);
         shadow += shadow_check(v3_add(ip.p, v3_create(-0.001, -0.001, -0.001)), vec_light);
+
+        // compute h
+        vec3 h = v3_normalize(v3_subtract(scene_lights[i].position, ip.i));
 
         if (shadow < 2) {
             if (light_cont > 0) {
                 cd_component += light_cont;
             }
+            // add (n*h)^alpha
             cs_component += scene_lights[i].intensity * pow((v3_dotprod(n, h)), alpha);
         }
     }
 
     vec3 cd_vec = v3_multiply(c_d, scene_ambient_light + k_d * cd_component);
     vec3 cs_vec = v3_multiply(c_s, k_s * cs_component);
-
     vec3 c_f = v3_add(cd_vec, cs_vec);
+    
     return c_f;
 }
 
@@ -97,14 +102,15 @@ shade_reflection(intersection_point ip)
 {
     vec3 matte_vector = shade_matte(ip);
 
+    // 2n(i x n) - i
     vec3 r = v3_multiply(v3_multiply(ip.n, 2), v3_dotprod(ip.i, ip.n));
     r = v3_subtract(r, ip.i);
 
+    // shoot two rays with offset to prevent black dots
     vec3 ray1 = v3_add(ip.p, v3_create(0.001, 0.001, 0.001));
     vec3 ray2 = v3_add(ip.p, v3_create(-0.001, -0.001, -0.001));
     vec3 reflection1 = ray_color(ip.ray_level + 1, ray1, r);
     vec3 reflection2 = ray_color(ip.ray_level + 1, ray2, r);
-
     vec3 reflective_vector = v3_add(v3_multiply(reflection1, 0.5),
                              v3_multiply(reflection2, 0.5));
 
